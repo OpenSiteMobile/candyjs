d1e16c8b7d01ff731f137f4d2ada86853237d5eb
Dwight Vietzke <dwight_vietzke@yahoo.com>
2015-03-21 21:27:24 -0400

    Remove Adsense, add site.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ msos.console.info('config -> start, (/candyjs/config.js file).');
 msos.console.time('config');
 
 // Set specific flags for this configuration:
-msos.config.run_ads = true;
+//msos.config.run_ads = true;
 msos.config.run_size = true;
 msos.config.run_analytics = true;
 //msos.config.run_onerror = true;
@@ -43,8 +43,8 @@ msos.config.use_websockets = typeof msos.config.query.use_websockets === 'boolea
 if (msos.config.debug_css) {
 	
 	msos.deferred_css = [
-		msos.resource_url('css', 'normalize.css'),
-		msos.resource_url('css', 'fawe.css'),
+		msos.resource_url('css', 'normalize.uc.css'),
+		msos.resource_url('css', 'font_awesome.uc.css'),
 		msos.resource_url('css', 'msos.css'),
 		msos.resource_url('css', 'msos_bs.css'),
 		msos.resource_url('css', 'msos_theme.css'),
@@ -54,7 +54,11 @@ if (msos.config.debug_css) {
 } else {
 
 	msos.deferred_css = [
-		msos.resource_url('css', 'bundle.min.css'),
+		msos.resource_url('css', 'normalize.min.css'),
+		msos.resource_url('css', 'font_awesome.min.css'),
+		msos.resource_url('css', 'msos.css'),
+		msos.resource_url('css', 'msos_bs.css'),
+		msos.resource_url('css', 'msos_theme.css'),
 		msos.resource_url('candy', 'css/chat.css')
 	];
 
@@ -73,9 +77,7 @@ if (msos.config.debug_script) {
 			msos.resource_url('modernizr', 'v283.uc.js'),		// no class selectors - see build.txt note in /htdocs/modernizr
 			msos.resource_url('jquery', 'v211.uc.js'),
 			msos.resource_url('jquery', 'ui/v1104.uc.js'),		// All UI Core + Draggable Interaction + Effects Core
-			msos.resource_url('underscore', 'v170.uc.js'),
-			msos.resource_url('hammer', 'v203.uc.js'),			// jQuery.hammer.js version of Hammer.js
-			msos.resource_url('backbone', 'v112.uc.js'),
+			msos.resource_url('underscore', 'v182.uc.js'),
 			msos.resource_url('strophe', 'md5.uc.js'),
 			msos.resource_url('strophe', 'v102ws.uc.js'),
 			msos.resource_url('strophe', 'muc.uc.js'),
@@ -91,9 +93,7 @@ if (msos.config.debug_script) {
 			msos.resource_url('modernizr', 'v283.uc.js'),		// no class selectors - see build.txt note in /htdocs/modernizr
 			msos.resource_url('jquery', 'v211.uc.js'),
 			msos.resource_url('jquery', 'ui/v1104.uc.js'),		// All UI Core + Draggable Interaction + Effects Core
-			msos.resource_url('underscore', 'v170.uc.js'),
-			msos.resource_url('hammer', 'v203.uc.js'),			// jQuery.hammer.js version of Hammer.js
-			msos.resource_url('backbone', 'v112.uc.js'),
+			msos.resource_url('underscore', 'v182.uc.js'),
 			msos.resource_url('strophe', 'base64.uc.js'),
 			msos.resource_url('strophe', 'md5.uc.js'),
 			msos.resource_url('strophe', 'v102.uc.js'),
@@ -109,9 +109,12 @@ if (msos.config.debug_script) {
 	if (msos.config.use_websockets && msos.config.websocket) {
 		// Standard site provided (including ext. bundles) scripts
 		msos.deferred_scripts = [
-			msos.resource_url('msos', 'bundle.min.js'),		// Modernizr, jQuery, jQuery-UI, Underscore, Backbone bundled together
+			msos.resource_url('modernizr', 'v283.min.js'),		// no class selectors - see build.txt note in /htdocs/modernizr
+			msos.resource_url('jquery', 'v211.min.js'),
+			msos.resource_url('jquery', 'ui/v1104.min.js'),		// All UI Core + Draggable Interaction + Effects Core
+			msos.resource_url('underscore', 'v182.min.js'),
 			msos.resource_url('strophe', 'md5.min.js'),
-			msos.resource_url('strophe', 'v102ws.uc.js'),	// Still under development
+			msos.resource_url('strophe', 'v102ws.uc.js'),		// Still under development
 			msos.resource_url('strophe', 'muc.min.js'),
 			msos.resource_url('mustache', 'v082.min.js'),
 			msos.resource_url('','site.js'),
@@ -122,10 +125,13 @@ if (msos.config.debug_script) {
 	} else {
 		// Standard site provided (including ext. bundles) scripts
 		msos.deferred_scripts = [
-			msos.resource_url('msos', 'bundle.min.js'),		// Modernizr, jQuery, jQuery-UI, Underscore, Backbone bundled together
+			msos.resource_url('modernizr', 'v283.min.js'),		// no class selectors - see build.txt note in /htdocs/modernizr
+			msos.resource_url('jquery', 'v211.min.js'),
+			msos.resource_url('jquery', 'ui/v1104.min.js'),		// All UI Core + Draggable Interaction + Effects Core
+			msos.resource_url('underscore', 'v182.min.js'),
 			msos.resource_url('strophe', 'base64.min.js'),
 			msos.resource_url('strophe', 'md5.min.js'),
-			msos.resource_url('strophe', 'v102.uc.js'),		// Still under development
+			msos.resource_url('strophe', 'v102.uc.js'),			// Still under development
 			msos.resource_url('strophe', 'muc.min.js'),
 			msos.resource_url('mustache', 'v082.min.js'),
 			msos.resource_url('','site.js'),
